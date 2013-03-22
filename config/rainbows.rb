910691e4c870ebaabe0538534bb3cfcbb88d5071
Luka Mirosevic <luka@goonbee.com>
2013-03-22 10:31:27 +0100

    12 workers, 1000 connections/worker

--- config/rainbows.rb
+++ config/rainbows.rb
@@ -1,10 +1,10 @@
-worker_processes 4
+worker_processes 12
 timeout 10
 preload_app true
 
 Rainbows! do
     use :EventMachine
-    worker_connections 400
+    worker_connections 1000
     client_max_body_size nil
     keepalive_timeout 0
 end
